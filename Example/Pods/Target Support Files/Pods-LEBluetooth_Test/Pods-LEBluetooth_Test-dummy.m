6f38b0bcde5fa38f322b8d27b0dfa2c4bd9a26e0
LarryEmerson <LarryEmerson@e-MacBook-Pro.lan>
2016-07-14 20:26:58 +0800

    pod  update

---
+++ Example/Pods/Target Support Files/Pods-LEBluetooth_Test/Pods-LEBluetooth_Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LEBluetooth_Test : NSObject
+@end
+@implementation PodsDummy_Pods_LEBluetooth_Test
+@end
--- Example/Pods/Target Support Files/Pods-LEBluetooth_Tests/Pods-LEBluetooth_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LEBluetooth_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_LEBluetooth_Tests
-@end
